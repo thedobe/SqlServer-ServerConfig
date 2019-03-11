eafbc8fee2992251684c85e20eb5517401979333
Michael <32280630+thedobe@users.noreply.github.com>
2019-03-11 06:36:53 -0400

    Update usp_server_configuration_create_tables.sql

--- usp_server_configuration_create_tables.sql
+++ usp_server_configuration_create_tables.sql
@@ -5,12 +5,13 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 -- =============================================
--- Author:		<Author,,Name>
+-- Author: <Author,,Name>
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE [dbo].[usp_database_configuration_create_tables]
+CREATE PROCEDURE [dbo].[usp_server_configuration_create_tables]
 	-- Add the parameters for the stored procedure here
 AS
 BEGIN
@@ -20,32 +21,53 @@ BEGIN
 
     -- Insert statements for procedure here
 
-	if not exists (select [name] from sys.tables where [name] = 'database_configuration_production_group')
-		create table [dbo].[database_configuration_production_group] (
+	if not exists (select [name] from sys.tables where [name] = 'server_configuration_production_group')
+		create table [dbo].[server_configuration_production_group] (
 			[id] [int] identity(1,1) NOT NULL,
 			[capture_date] [datetime] default getdate() NOT NULL,
 			[server_name] [sysname] NULL,
+			[database_count] [int] NULL,
+			[edition] [varchar](255) NULL,
+			[version] [varchar](50) NULL,
+			[service_pack] [varchar](10) NULL,
+			[processors] [tinyint] NULL,
+			[physical_memory_gb] [int] NULL,
+			[windows_version] [varchar](250) NULL,
+			[min_server_memory_gb] [int] NULL,
+			[max_server_memory_gb] [int] NULL,
+			[login_mode] [varchar](50) NULL,
+			[fill_factor] [tinyint] NULL,
+			[is_backup_compression] [bit] NULL,
+			[root_directory] [varchar](1000) NULL,
+			[backup_directory] [varchar](1000) NULL,
+			[log_directory] [varchar](1000) NULL,
+			[data_directory] [varchar](1000) NULL,
+			[is_adhoc_workload] [bit] NULL,
+			[max_dop] [tinyint] NULL,
+			[cost_threshold] [tinyint] NULL,
+			[is_trace_flag] [varchar](1000) NULL,
+			[is_enterprise_feature] [bit] NULL,
+			primary key clustered ([id] asc) 
+	)
+
+	if not exists (select [name] from sys.tables where [name] = 'server_configuration_trace_flag_production_group')
+		create table [dbo].[server_configuration_trace_flag_production_group] (
+			[id] [int] identity(1,1) NOT NULL,
+			[capture_date] [datetime] default getdate() NOT NULL,
+			[server_name] [sysname] NULL,
+			[trace_flag] [int]
+			primary key clustered ([id] asc) 
+	)
+
+	if not exists (select [name] from sys.tables where [name] = 'server_configuration_sku_production_group')
+		create table [dbo].[server_configuration_sku_production_group] (
+			[id] [int] identity(1,1) NOT NULL,
+			[capture_date] [datetime] default getdate() NOT NULL,
+			[server_name] [sysname] NULL,
+			[instance_name] [sysname] NULL,
 			[database_name] [sysname] NULL,
-			[last_backup] [datetime] NULL,
-			[ROWS_total_size_gb] [int] NULL,
-			[ROWS_files] [int] NULL,
-			[ROWS_p_growth] [tinyint] NULL,
-			[LOG_total_size_gb] [int] NULL,
-			[LOG_p_growth] [tinyint] NULL,
-			[owner_sid] [varbinary](256) NULL,
-			[sysuser_loginname] [sysname] NULL,
-			[compatibility_level] [tinyint] NULL,
-			[collation_name] [sysname] NULL,
-			[user_access] [varchar](60) NULL,
-			[recovery_model] [varchar](60) NULL,
-			[is_auto_shrink_on] [tinyint] NULL,
-			[state] [varchar](60) NULL,
-			[snapshot_isolation_state] [tinyint] NULL,
-			[is_read_committed_snapshot_on] [tinyint] NULL,
-			[page_verify_option] [varchar](60) NULL,
-			[is_auto_create_stats_on] [tinyint] NULL,
-			[is_auto_update_stats_on] [tinyint] NULL,
-			[is_encrypted] [tinyint] NULL
+			[feature_id] [varchar](25) NULL,
+			[feature_name] [varchar](250) NULL
 			primary key clustered ([id] asc) 
 	)
 END
